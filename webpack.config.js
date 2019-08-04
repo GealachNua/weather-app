19f0ce683b8154aad321c7d7ed07093f41d8075e
Christoph Wiekart <cwiekart@xs4all.nl>
2019-08-04 15:58:53 +0200

    Added babel-loader@7 and corrected clean-webpack-plugin in webpack file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -45,7 +45,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin("dist", {}),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css"
     }),
