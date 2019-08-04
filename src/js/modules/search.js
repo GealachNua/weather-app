dff0b957406bdd2c7d9bfc49698d862944174a71
Christoph Wiekart <cwiekart@xs4all.nl>
2019-08-04 19:48:39 +0200

    Use Google Geocoding API to fetch lat/long of address. API key to be added.

--- src/js/modules/search.js
+++ src/js/modules/search.js
@@ -5,11 +5,12 @@ const $searchForm = document.querySelector(".search__form");
 const $searchInput = document.querySelector(".search__input");
 const $searchCity = document.querySelector(".search__city");
 const $spinnerWrapper = document.querySelector(".spinner-wrapper");
+const GEOCODE_KEY = "tbd";
 
 export const initSearch = () => {
     console.log("Hi this is search module speaking");
     bindSearchEvents();
-} 
+}; 
 
 const bindSearchEvents = () => {
     $searchForm.addEventListener("submit", e => {
@@ -19,9 +20,23 @@ const bindSearchEvents = () => {
         if ($searchInput.value === "" ) return;
         address = $searchInput.value;
         $searchInput.value = "";
+        getLatLng(address);
         render();
-    })
-} 
+    });
+}; 
+
+// Get lat long from Google Geocoding API.
+const getLatLng = async (query) => {
+    const reqLink =
+        `https://maps.googleapis.com/maps/api/geocode/json?address=4{query}&key=${GEOCODE_KEY}`;
+    const fetchData = await fetch(reqLink);
+    const parsed = await fetchData.json();
+    const latLng = {
+        lat: parsed[0].geometry.location.lat,
+        lng: parsed[0].geometry.location.lng
+    };
+    return latLng;
+};
 
 const render = () => {
     $searchCity.innerHTML = address;
